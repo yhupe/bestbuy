d3bb4090c354707f9c73761d5ea15ef56cd08508
yhupe <hannes.pickel@outlook.com>
2025-02-24 13:51:33 +0100

    commit before a break

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 from products import Product
 from store import Store
 
-product_list = [Product("MacBook Air M2", price=1450, quantity=100),
-                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                Product("Google Pixel 7", price=500, quantity=250),
+product_list = [ Product("MacBook Air M2", price=1450, quantity=100),
+                 Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                 Product("Google Pixel 7", price=500, quantity=250)
                ]
-
 best_buy = Store(product_list)
-products = best_buy.get_all_products()
-print(best_buy.get_total_quantity())
-print(best_buy.order([(products[0], 1), (products[2], 2)]))
 
 
+def start(store):
 
+    while True:
+
+        print('''
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+        ''')
+        print()
+        user_input = input("Please choose a number: ")
+        print()
+
+        if user_input == "1":
+
+
+        if user_input == "2":
+            store.get_total_quantity()
+
+        if user_input == "3":
+            print(store.get_all_products())
+            print()
+            user_order_product = input("")
+            user_order_quantity = input("")
+            store.order(user_order_quantity)
+
+        if user_input =="4":
+            break
+
+start(best_buy)
 
