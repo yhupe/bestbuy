e77b6c4171b77c39226c3e62e7ebc10b29ae113a
yhupe <hannes.pickel@outlook.com>
2025-02-24 13:24:21 +0100

    create Store class

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from products import Product
+from store import Store
 
-mac = Product("MacBook Air M2", price=1450, quantity=100)
+product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                Product("Google Pixel 7", price=500, quantity=250),
+               ]
+
+best_buy = Store(product_list)
+products = best_buy.get_all_products()
+print(best_buy.get_total_quantity())
+print(best_buy.order([(products[0], 1), (products[2], 2)]))
+
 
 
 
---
+++ store.py
@@ -0,0 +1,36 @@
+import products
+
+class Store:
+    """ It seems to be best practice to set iterable objects to none """
+
+    def __init__(self, products= None):
+        if products is None:
+            products = []
+        self.products = products
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self):
+
+        counter = 0
+
+        for product in self.products:
+            counter += products.Product.get_quantity(product)
+
+        return counter
+
+    def get_all_products(self):
+        return self.products
+
+    def order(self, shopping_list):
+
+        total_price = 0
+
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+
+        return total_price
